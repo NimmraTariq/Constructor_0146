748626ff6e7407179c8e0f0bd58c92a7453aa319
NimmraTariq <nimra369tariq@gmail.com>
2025-05-17 19:11:42 +0700

    Constructor

--- Destructor.cpp
+++ Destructor.cpp
@@ -14,8 +14,8 @@ public:
 };
 
 // Definisi member Function
-angka::angka(int i) { // Constructor
-    panjang = i;
+angka::angka(int i)  { // Constructor
+    panjang = i; 
     arr = new int[i];
     isiData();
 }
