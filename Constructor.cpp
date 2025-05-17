ca96317460d8a8dce746d4f0e53161daef403c9b
NimmraTariq <nimra369tariq@gmail.com>
2025-05-17 18:45:38 +0700

    Constructor with string parameters

--- Constructor.cpp
+++ Constructor.cpp
@@ -10,7 +10,7 @@ private:
 public:
     mahasiswa();                        // Default constructor
     mahasiswa(int);                     // Constructor dengan parameter int
-    mahasiswa(string);                   // Constructor dengan parameter string
+    mahasiswa(string) ;                  // Constructor dengan parameter string
     mahasiswa(int iNim, string iNama);   // Constructor dengan 2 parameter
     void cetak();                        // Fungsi cetak
 };
